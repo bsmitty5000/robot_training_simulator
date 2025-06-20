571c794787a80f9d11b2db8b4377467daabff5aa
Brian <briandsmith1@gmail.com>
2025-06-20 11:06:20 -0400

    huge reorg 2

--- models/obstacle.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, color=(100, 100, 100)):
-        super().__init__()
-        self.image = pygame.Surface((width, height))
-        self.image.fill(color)
-        self.rect = self.image.get_rect(topleft=(x, y))
